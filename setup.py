bc4ef71549ffd823c8db939d7bff6f9dbc32767d
Your Name <your.email@example.com>
2025-04-06 20:59:32 +0200

    first commit 1.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyremoteview",
-    version="0.1.1",
+    version="1.0.0",
     description="Remote SSH Image Gallery Viewer",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
